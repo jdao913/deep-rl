1340b45624b84f84fc1989db589e8a1a91dc36ad
Jeremy Dao <jeremydao913@gmail.com>
2019-09-25 12:26:13 -0700

    added envs to master branch

---
+++ cassie/standing_env.py
@@ -0,0 +1,326 @@
+from .cassiemujoco import pd_in_t, state_out_t, CassieSim, CassieVis
+
+from .trajectory import CassieTrajectory
+
+from math import floor
+
+import numpy as np 
+import os
+import random
+
+import pickle
+
+class CassieIKTrajectory:
+    def __init__(self, filepath):
+        with open(filepath, "rb") as f:
+            trajectory = pickle.load(f)
+
+        self.qpos = np.copy(trajectory["qpos"])
+        self.qvel = np.copy(trajectory["qvel"])
+        #self.foot =
+    
+    def __len__(self):
+        return len(self.qpos)
+
+class CassieEnv_stand:
+    def __init__(self, simrate=60, state_est=False):
+        self.sim = CassieSim("./cassie/cassiemujoco/cassie.xml")
+        self.vis = None
+
+        self.state_est = state_est
+
+        self.observation_space = np.zeros(40)
+        if self.state_est:
+            self.observation_space = np.zeros(46)       # Size for use with state est
+
+        self.action_space      = np.zeros(10)
+
+        self.P = np.array([100,  100,  88,  96,  50]) 
+        self.D = np.array([10.0, 10.0, 8.0, 9.6, 5.0])
+
+        self.u = pd_in_t()
+
+        dirname = os.path.dirname(__file__)
+        traj_path = os.path.join(dirname, "trajectory", "more-poses-trial.bin")
+        self.trajectory = CassieTrajectory(traj_path)
+
+        # TODO: should probably initialize this to current state
+        self.cassie_state = state_out_t()
+
+        self.simrate = simrate # simulate X mujoco steps with same pd target
+                               # 60 brings simulation from 2000Hz to roughly 30Hz
+        self.phaselen = floor(len(self.trajectory) / self.simrate) - 1
+
+        self.time    = 0 # number of time steps in current episode
+        self.push_time = -1
+
+        # see include/cassiemujoco.h for meaning of these indices
+        self.pos_idx = [7, 8, 9, 14, 20, 21, 22, 23, 28, 34]
+        self.vel_idx = [6, 7, 8, 12, 18, 19, 20, 21, 25, 31]
+    
+    def step_simulation(self, action):
+
+        real_action = action
+        offset = np.array([0.0045, 0.0, 0.4973, -1.1997, -1.5968, 0.0045, 0.0, 0.4973, -1.1997, -1.5968])
+        real_action = real_action + offset
+        # real_action[4] += -1.5968
+        # real_action[9] += -1.5968
+        
+        # target = action + ref_pos[self.pos_idx]
+        
+        self.u = pd_in_t()
+        for i in range(5):
+            # TODO: move setting gains out of the loop?
+            # maybe write a wrapper for pd_in_t ?
+            self.u.leftLeg.motorPd.pGain[i]  = self.P[i]
+            self.u.rightLeg.motorPd.pGain[i] = self.P[i]
+
+            self.u.leftLeg.motorPd.dGain[i]  = self.D[i]
+            self.u.rightLeg.motorPd.dGain[i] = self.D[i]
+
+            self.u.leftLeg.motorPd.torque[i]  = 0 # Feedforward torque
+            self.u.rightLeg.motorPd.torque[i] = 0 
+
+            self.u.leftLeg.motorPd.pTarget[i]  = real_action[i]
+            self.u.rightLeg.motorPd.pTarget[i] = real_action[i + 5]
+
+            self.u.leftLeg.motorPd.dTarget[i]  = 0
+            self.u.rightLeg.motorPd.dTarget[i] = 0
+
+        self.cassie_state = self.sim.step_pd(self.u)
+
+    def step(self, action):
+        for _ in range(self.simrate):
+            self.step_simulation(action)
+
+        height = self.sim.qpos()[2]
+
+        self.time  += 1
+        if self.time == self.push_time:
+            push = 150#random.randint(-20, 20) / 10.0
+            push_dir = random.randint(0, 2)
+            force_arr = np.zeros(6)
+            force_arr[push_dir] = push
+            self.sim.apply_force(force_arr)
+
+        # Early termination
+        done = not(height > 0.4 and height < 3.0)
+
+        reward = self.compute_reward()
+
+        # TODO: make 0.3 a variable/more transparent
+        if reward < 0.3:
+            done = True
+
+        return self.get_full_state(), reward, done, {}
+
+    def reset(self):
+        self.phase = random.randint(0, self.phaselen)
+        self.time = 0
+        self.counter = 0
+
+        qpos, qvel = self.get_ref_state(self.phase)
+        reset_qpos = [0.0045, 0, 0.4973, 0.9785, -0.0164, 0.01787, -0.2049,
+                    -1.1997, 0, 1.4267, 0, -1.5244, 1.5244, -1.5968,
+                    -0.0045, 0, 0.4973, 0.9786, 0.00386, -0.01524, -0.2051,
+                    -1.1997, 0, 1.4267, 0, -1.5244, 1.5244, -1.5968]
+        qvel = np.zeros(32)
+        # qpos[2] -= .1
+
+        self.sim.set_qpos(qpos)
+        self.sim.set_qvel(qvel)
+
+        # Need to reset u? Or better way to reset cassie_state than taking step
+        self.cassie_state = self.sim.step_pd(self.u)
+
+        #self.push_time = random.randrange(30, 100)
+        push = 100#random.randint(-20, 20) / 10.0
+        push_dir = random.randint(0, 2)
+        force_arr = np.zeros(6)
+        force_arr[push_dir] = push
+        self.sim.apply_force(force_arr)
+
+        return self.get_full_state()
+
+    # used for plotting against the reference trajectory
+    def reset_for_test(self):
+        self.phase = 0
+        self.time = 0
+        self.counter = 0
+        self.speed = 1
+
+        qpos, qvel = self.get_ref_state(self.phase)
+
+        self.sim.set_qpos(qpos)
+        self.sim.set_qvel(qvel)
+
+        # Need to reset u? Or better way to reset cassie_state than taking step
+        self.cassie_state = self.sim.step_pd(self.u)
+
+        return self.get_full_state()
+    
+    def set_joint_pos(self, jpos, fbpos=None, iters=5000):
+        """
+        Kind of hackish. 
+        This takes a floating base position and some joint positions
+        and abuses the MuJoCo solver to get the constrained forward
+        kinematics. 
+
+        There might be a better way to do this, e.g. using mj_kinematics
+        """
+
+        # actuated joint indices
+        joint_idx = [7, 8, 9, 14, 20,
+                     21, 22, 23, 28, 34]
+
+        # floating base indices
+        fb_idx = [0, 1, 2, 3, 4, 5, 6]
+
+        for _ in range(iters):
+            qpos = np.copy(self.sim.qpos())
+            qvel = np.copy(self.sim.qvel())
+
+            qpos[joint_idx] = jpos
+
+            if fbpos is not None:
+                qpos[fb_idx] = fbpos
+
+            self.sim.set_qpos(qpos)
+            self.sim.set_qvel(0 * qvel)
+
+            self.sim.step_pd(pd_in_t())
+
+
+    # NOTE: this reward is slightly different from the one in Xie et al
+    # see notes for details
+    def compute_reward(self):
+        qpos = np.copy(self.sim.qpos())
+        qvel = np.copy(self.sim.qvel())
+
+        stand_pos = np.array([0.0, 0.0, 1.01, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.978483, -0.016400, 0.017870, -0.204896, 0.0, 0.0, 1.426700, 0.0, 
+        -1.524400, 1.524400, 0.0, 0.0, 0.0, 0.0, 0.978614, 0.003860, -0.015240, -0.205103, 0.0, 0.0, 1.426700, 0.0, -1.524400, 1.524400, 0.0])
+        
+        height_diff = np.abs(qpos[2] - .95)
+        reward = np.exp(-height_diff)
+
+        return reward
+
+    # get the corresponding state from the reference trajectory for the current phase
+    def get_ref_state(self, phase=None):
+        if phase is None:
+            phase = self.phase
+
+        if phase > self.phaselen:
+            phase = 0
+
+        pos = np.copy(self.trajectory.qpos[phase * self.simrate])
+
+        # this is just setting the x to where it "should" be given the number
+        # of cycles
+        # pos[0] += (self.trajectory.qpos[-1, 0] - self.trajectory.qpos[0, 0]) * self.counter
+        
+        # ^ should only matter for COM error calculation,
+        # gets dropped out of state variable for input reasons
+
+        ###### Setting variable speed  #########
+        # pos[0] *= self.speed
+        # pos[0] += (self.trajectory.qpos[-1, 0]- self.trajectory.qpos[0, 0])* self.counter * self.speed
+        ######                          ########
+
+        # setting lateral distance target to 0?
+        # regardless of reference trajectory?
+        pos[1] = 0
+
+        vel = np.copy(self.trajectory.qvel[phase * self.simrate])
+        # vel[0] *= self.speed
+
+        return pos, vel
+
+    def get_full_state(self):
+        qpos = np.copy(self.sim.qpos())
+        qvel = np.copy(self.sim.qvel()) 
+
+        # TODO: maybe convert to set subtraction for clarity
+        # {i for i in range(35)} - 
+        # {0, 10, 11, 12, 13, 17, 18, 19, 24, 25, 26, 27, 31, 32, 33}
+
+        # this is everything except pelvis x and qw, achilles rod quaternions, 
+        # and heel spring/foot crank/plantar rod angles
+        # note: x is forward dist, y is lateral dist, z is height
+
+        # makes sense to always exclude x because it is in global coordinates and
+        # irrelevant to phase-based control. Z is inherently invariant to
+        # trajectory despite being global coord. Y is only invariant to straight
+        # line trajectories.
+
+        # [ 0] Pelvis y
+        # [ 1] Pelvis z
+        # [ 2] Pelvis orientation qw
+        # [ 3] Pelvis orientation qx
+        # [ 4] Pelvis orientation qy
+        # [ 5] Pelvis orientation qz
+        # [ 6] Left hip roll         (Motor [0])
+        # [ 7] Left hip yaw          (Motor [1])
+        # [ 8] Left hip pitch        (Motor [2])
+        # [ 9] Left knee             (Motor [3])
+        # [10] Left shin                        (Joint [0])
+        # [11] Left tarsus                      (Joint [1])
+        # [12] Left foot             (Motor [4], Joint [2])
+        # [13] Right hip roll        (Motor [5])
+        # [14] Right hip yaw         (Motor [6])
+        # [15] Right hip pitch       (Motor [7])
+        # [16] Right knee            (Motor [8])
+        # [17] Right shin                       (Joint [3])
+        # [18] Right tarsus                     (Joint [4])
+        # [19] Right foot            (Motor [9], Joint [5])
+        pos_index = np.array([1,2,3,4,5,6,7,8,9,14,15,16,20,21,22,23,28,29,30,34])
+
+        # [ 0] Pelvis x
+        # [ 1] Pelvis y
+        # [ 2] Pelvis z
+        # [ 3] Pelvis orientation wx
+        # [ 4] Pelvis orientation wy
+        # [ 5] Pelvis orientation wz
+        # [ 6] Left hip roll         (Motor [0])
+        # [ 7] Left hip yaw          (Motor [1])
+        # [ 8] Left hip pitch        (Motor [2])
+        # [ 9] Left knee             (Motor [3])
+        # [10] Left shin                        (Joint [0])
+        # [11] Left tarsus                      (Joint [1])
+        # [12] Left foot             (Motor [4], Joint [2])
+        # [13] Right hip roll        (Motor [5])
+        # [14] Right hip yaw         (Motor [6])
+        # [15] Right hip pitch       (Motor [7])
+        # [16] Right knee            (Motor [8])
+        # [17] Right shin                       (Joint [3])
+        # [18] Right tarsus                     (Joint [4])
+        # [19] Right foot            (Motor [9], Joint [5])
+        vel_index = np.array([0,1,2,3,4,5,6,7,8,12,13,14,18,19,20,21,25,26,27,31])
+
+        # Use state estimator
+        robot_state = np.concatenate([
+            [self.cassie_state.pelvis.position[2] - self.cassie_state.terrain.height], # pelvis height
+            self.cassie_state.pelvis.orientation[:],                                 # pelvis orientation
+            self.cassie_state.motor.position[:],                                     # actuated joint positions
+
+            self.cassie_state.pelvis.translationalVelocity[:],                       # pelvis translational velocity
+            self.cassie_state.pelvis.rotationalVelocity[:],                          # pelvis rotational velocity 
+            self.cassie_state.motor.velocity[:],                                     # actuated joint velocities
+
+            self.cassie_state.pelvis.translationalAcceleration[:],                   # pelvis translational acceleration
+            
+            self.cassie_state.joint.position[:],                                     # unactuated joint positions
+            self.cassie_state.joint.velocity[:]                                      # unactuated joint velocities
+        ])
+
+        if self.state_est:
+            return robot_state
+        else:
+            return np.concatenate([qpos[pos_index], 
+                               qvel[vel_index]])
+
+    def render(self):
+        if self.vis is None:
+            self.vis = CassieVis(self.sim, "./cassie/cassiemujoco/cassie.xml")
+
+        return self.vis.draw(self.sim)
